1d0dac5415ed15dcc143ae0da05a0ac4675e4bac
nitish chavan <nitishkchavan@gmail.com>
2017-09-14 14:27:46 +0530

    turtle list page completed

---
+++ js/config.js
@@ -0,0 +1,17 @@
+application.config(function($routeProvider){
+
+    /*$routeProvider
+        .when('/',{
+            templateUrl:'partials/login.html',
+            controller:'angular_controller'
+        }).when('/dashboard',{
+            templateUrl:'partials/dashboard.html',
+            controller:'angular_controller'
+        }).when('/reports',{
+            templateUrl:'partials/reports.html',
+            controller:'angular_controller'
+        }).when('/login',{
+            templateUrl:'partials/login.html',
+            controller:'angular_controller'
+        });*/
+});
